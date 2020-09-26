c33638aac7873bf6362ecc13acb8a7a4cf7b3912
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-09-26 18:51:14 +0200

    adjusted return and input types for get_attacked_squares

--- src/attack.rs
+++ src/attack.rs
@@ -2,34 +2,31 @@ use crate::color;
 use crate::pieces::{pawn, piece, position};
 use std::collections;
 
-fn get_attacked_squares(
-    board: [[Box<dyn piece::Piece>; 8]; 8],
+fn get_attacked_squares<'a>(
+    board: &Vec<Vec<Option<Box<dyn piece::Piece>>>>,
     color: color::Color,
-) -> Vec<Vec<Option<AttackedSquare>>> {
-    let mut map: collections::HashMap<&str, Vec<Box<dyn piece::Piece>>> =
-        collections::HashMap::new();
-
-    let p = pawn::Pawn {
-        position: position::Position(2, 2),
-        color: color::Color::WHITE,
-    };
-
-    match map.get_mut("01") {
-        None => {
-            map.insert("01", vec![Box::new(p)]);
-        }
-        Some(v) => v.push(Box::new(p)),
-    }
-
-    for file in &board {
-        for piece in file {
-            if *piece.color() == color {}
-        }
+) -> Vec<Vec<Option<Vec<&'a Box<dyn piece::Piece>>>>> {
+    if board.len() != 8 || board.iter().any(|x| x.len() != 8) { 
+        panic!("Invalid board dimensions");
     }
 
     vec![]
 }
 
-pub struct AttackedSquare {
-    pub attackers: Vec<Box<dyn piece::Piece>>,
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "Invalid board dimensions")]
+    fn invalid_file_size() {
+        get_attacked_squares(&vec!(), color::Color::BLACK);
+    }
+
+    #[test]
+    #[should_panic(expected = "Invalid board dimensions")]
+    fn invalid_rank_size() {
+        get_attacked_squares(&vec!(vec!(),vec!(),vec!(),vec!(),vec!(),vec!(),vec!()), color::Color::BLACK);
+    }
 }
