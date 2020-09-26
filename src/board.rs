cf20133285749c4006d6465c1f44f1da0dab5a23
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-09-27 01:51:42 +0200

    forgot to commit the actual board.rs file

---
+++ src/board.rs
@@ -0,0 +1,32 @@
+use super::pieces::{piece, position};
+use std::slice;
+
+pub struct Board {
+    board: Vec<Vec<Option<Box<dyn piece::Piece>>>>,
+}
+
+impl Board {
+    pub fn empty() -> Self {
+        let mut empty_board: Vec<Vec<Option<Box<dyn piece::Piece>>>> = vec![];
+        for _ in 0..8 {
+            let mut empty_file: Vec<Option<Box<dyn piece::Piece>>> = vec![];
+            for _ in 0..8 {
+                empty_file.push(None);
+            }
+            empty_board.push(empty_file);
+        }
+        Self { board: empty_board }
+    }
+
+    pub fn iter(&self) -> slice::Iter<Vec<Option<Box<dyn piece::Piece>>>> {
+        self.board.iter()
+    }
+
+    pub fn set_square(
+        &mut self,
+        square: Option<Box<dyn piece::Piece>>,
+        position: position::Position,
+    ) {
+        self.board[position.0 as usize - 1][position.1 as usize - 1] = square;
+    }
+}
