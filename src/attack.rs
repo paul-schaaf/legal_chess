85a1634f4bcbea77ab273ad49ad405311ea25f22
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-09-28 11:04:19 +0200

    tests for attacked squares with initial board

--- src/attack.rs
+++ src/attack.rs
@@ -23,7 +23,7 @@ pub fn get_attacked_squares(
                     let attacked_positions = piece.attacks(board);
                     attacked_positions.iter().for_each(|position| {
                         let square =
-                            attacked_board[position.0 as usize][position.1 as usize].take();
+                            attacked_board[position.0 as usize - 1][position.1 as usize - 1].take();
                         let new_square = match square {
                             None => Some(vec![piece]),
                             Some(mut v) => {
@@ -44,7 +44,7 @@ pub fn get_attacked_squares(
 
 #[cfg(test)]
 mod tests {
-    use super::super::pieces::{pawn, position};
+    use super::super::pieces::{pawn, piece, position};
     use super::*;
 
     #[test]
@@ -75,5 +75,164 @@ mod tests {
         };
 
         assert_eq!(pawn_id, actual.get_id());
+    }
+
+    #[test]
+    fn initial_board_attacks_white() {
+        initial_board_attacks(color::Color::WHITE);
+    }
+
+    #[test]
+    fn initial_board_attacks_black() {
+        initial_board_attacks(color::Color::BLACK);
+    }
+
+    fn initial_board_attacks(color: color::Color) {
+        let board = board::Board::initial();
+        let attacked_squares = get_attacked_squares(&board, color);
+
+        for i in 0..8 {
+            match (&attacked_squares[i][3], &attacked_squares[i][4]) {
+                (None, None) => (),
+                _ => panic!("Should've been: None"),
+            }
+        }
+
+        let modifier = if color == color::Color::WHITE { 0 } else { 3 };
+
+        assert_attacked_by(
+            &attacked_squares[0][2 + modifier],
+            vec![(piece::PieceEnum::PAWN, 1), (piece::PieceEnum::KNIGHT, 1)],
+        );
+        assert_attacked_by(
+            &attacked_squares[1][2 + modifier],
+            vec![(piece::PieceEnum::PAWN, 2)],
+        );
+        assert_attacked_by(
+            &attacked_squares[2][2 + modifier],
+            vec![(piece::PieceEnum::PAWN, 2), (piece::PieceEnum::KNIGHT, 1)],
+        );
+        assert_attacked_by(
+            &attacked_squares[3][2 + modifier],
+            vec![(piece::PieceEnum::PAWN, 2)],
+        );
+        assert_attacked_by(
+            &attacked_squares[4][2 + modifier],
+            vec![(piece::PieceEnum::PAWN, 2)],
+        );
+        assert_attacked_by(
+            &attacked_squares[5][2 + modifier],
+            vec![(piece::PieceEnum::PAWN, 2), (piece::PieceEnum::KNIGHT, 1)],
+        );
+        assert_attacked_by(
+            &attacked_squares[6][2 + modifier],
+            vec![(piece::PieceEnum::PAWN, 2)],
+        );
+        assert_attacked_by(
+            &attacked_squares[7][2 + modifier],
+            vec![(piece::PieceEnum::PAWN, 1), (piece::PieceEnum::KNIGHT, 1)],
+        );
+
+        let modifier = if color == color::Color::WHITE { 0 } else { 5 };
+
+        assert_attacked_by(
+            &attacked_squares[0][1 + modifier],
+            vec![(piece::PieceEnum::ROOK, 1)],
+        );
+        assert_attacked_by(
+            &attacked_squares[1][1 + modifier],
+            vec![(piece::PieceEnum::BISHOP, 1)],
+        );
+        assert_attacked_by(
+            &attacked_squares[2][1 + modifier],
+            vec![(piece::PieceEnum::QUEEN, 1)],
+        );
+        assert_attacked_by(
+            &attacked_squares[3][1 + modifier],
+            vec![
+                (piece::PieceEnum::QUEEN, 1),
+                (piece::PieceEnum::BISHOP, 1),
+                (piece::PieceEnum::KING, 1),
+                (piece::PieceEnum::KNIGHT, 1),
+            ],
+        );
+        assert_attacked_by(
+            &attacked_squares[4][1 + modifier],
+            vec![
+                (piece::PieceEnum::QUEEN, 1),
+                (piece::PieceEnum::BISHOP, 1),
+                (piece::PieceEnum::KING, 1),
+                (piece::PieceEnum::KNIGHT, 1),
+            ],
+        );
+        assert_attacked_by(
+            &attacked_squares[5][1 + modifier],
+            vec![(piece::PieceEnum::KING, 1)],
+        );
+        assert_attacked_by(
+            &attacked_squares[6][1 + modifier],
+            vec![(piece::PieceEnum::BISHOP, 1)],
+        );
+        assert_attacked_by(
+            &attacked_squares[7][1 + modifier],
+            vec![(piece::PieceEnum::ROOK, 1)],
+        );
+
+        let modifier = if color == color::Color::WHITE { 0 } else { 7 };
+
+        assert_attacked_by(&attacked_squares[0][0 + modifier], vec![]);
+        assert_attacked_by(
+            &attacked_squares[1][0 + modifier],
+            vec![(piece::PieceEnum::ROOK, 1)],
+        );
+        assert_attacked_by(
+            &attacked_squares[2][0 + modifier],
+            vec![(piece::PieceEnum::QUEEN, 1)],
+        );
+        assert_attacked_by(
+            &attacked_squares[3][0 + modifier],
+            vec![(piece::PieceEnum::KING, 1)],
+        );
+        assert_attacked_by(
+            &attacked_squares[4][0 + modifier],
+            vec![(piece::PieceEnum::QUEEN, 1)],
+        );
+        assert_attacked_by(
+            &attacked_squares[5][0 + modifier],
+            vec![(piece::PieceEnum::KING, 1)],
+        );
+        assert_attacked_by(
+            &attacked_squares[6][0 + modifier],
+            vec![(piece::PieceEnum::ROOK, 1)],
+        );
+        assert_attacked_by(&attacked_squares[7][0 + modifier], vec![]);
+    }
+
+    fn assert_attacked_by(
+        square: &std::option::Option<std::vec::Vec<&std::boxed::Box<dyn piece::Piece>>>,
+        attackers: Vec<(piece::PieceEnum, u8)>,
+    ) {
+        match square {
+            Some(v) => {
+                let expected_sum_of_attackers =
+                    attackers.iter().fold(0, |acc, attacker| acc + attacker.1);
+                assert_eq!(expected_sum_of_attackers as usize, v.len());
+                for attacker in attackers {
+                    let attacking_pieces = v
+                        .iter()
+                        .filter(|v| v.piece() == attacker.0)
+                        .collect::<Vec<_>>();
+                    assert_eq!(attacker.1 as usize, attacking_pieces.len());
+                    for piece in attacking_pieces {
+                        assert_eq!(attacker.0, piece.piece());
+                    }
+                }
+            }
+            _ => {
+                if attackers.len() != 0 {
+                    panic!("Should have no attackers but had some.")
+                }
+            }
+        }
     }
 }
