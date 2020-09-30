aed8bed98102b5d8ce961c9a05dc32b179b78642
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-09-30 21:34:48 +0200

    cleaned up dead code

--- src/lib.rs
+++ src/lib.rs
@@ -4,30 +4,3 @@ mod chessmove;
 pub mod color;
 pub mod game;
 pub mod pieces;
-
-fn legal_moves(board: [[&str; 8]; 8]) -> Vec<chessmove::ChessMove> {
-    vec![chessmove::ChessMove {
-        source_rank: 2,
-        source_file: 2,
-        target_rank: 4,
-        target_file: 2,
-    }]
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn legal_moves_ok() {
-        let moves = [["p"; 8]; 8];
-        let result = vec![chessmove::ChessMove {
-            source_rank: 2,
-            source_file: 2,
-            target_rank: 4,
-            target_file: 2,
-        }];
-
-        assert_eq!(result, legal_moves(moves));
-    }
-}
